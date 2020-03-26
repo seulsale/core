0e8bcad64fd1cedaad7dd9e00ca4f992bdbfe5a6
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-03-26 04:14:28 -0600

    Add server ip

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ["DEBUG"] == "True" else False
 
-ALLOWED_HOSTS = ['core-dev2.us-west-2.elasticbeanstalk.com', '127.0.0.1', 'core.seulsale.com']
+ALLOWED_HOSTS = ['core-dev2.us-west-2.elasticbeanstalk.com', '127.0.0.1', 'core.seulsale.com', '54.69.26.132']
 
 # Application definition
 
