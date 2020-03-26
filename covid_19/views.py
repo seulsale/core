e17d9974b71026159ececaa43a9caa308004e720
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-03-26 02:30:10 -0600

    Add dev host header

--- covid_19/views.py
+++ covid_19/views.py
@@ -17,19 +17,23 @@ def covid_data(request):
         recovered_data = pd.read_csv(recovered_url, error_bad_lines=False)
 
         countries = confirmed_data['Country/Region'].to_numpy()
-        hold_data = {}
+        hold_data = []
+        saved_countries = []
 
         for country in countries:
-            if country not in hold_data.keys():
-                hold_data[country] = {
+            if country not in saved_countries:
+                saved_countries.append(country)
+                hold_data.append({
+                    'name': country,
                     'confirmed': confirmed_data.loc[
                         confirmed_data['Country/Region'] == country, confirmed_data.keys()[-1]].sum(),
                     'deaths': deaths_data.loc[deaths_data['Country/Region'] == country, deaths_data.keys()[-1]].sum(),
                     'recovered': recovered_data.loc[
                         recovered_data['Country/Region'] == country, recovered_data.keys()[-1]].sum(),
-                }
+                })
         return Response(hold_data, HTTP_200_OK)
     except Exception as e:
+        print(e)
         return Response({
             'message': str(e)
         }, HTTP_500_INTERNAL_SERVER_ERROR)
