ba5e1bfdf4387e4ac68c79c13c45648a61970717
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-03-26 02:11:42 -0600

    Add allowed host

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ["DEBUG"] == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['core-dev2.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
